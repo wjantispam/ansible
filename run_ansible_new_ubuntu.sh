c0eccee2e0007d9c35df5983cc44a9a5e8c903b2
WJ <wj.antispam@gmail.com>
2024-01-08 22:39:16 +0000

    feature/new ubuntu installations

--- run_ansible_new_ubuntu.sh
+++ run_ansible_new_ubuntu.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt install ansible
+if $(add-apt-repository --list | grep -q ansible); 
+then 
+	echo "Ansible repository exists"; 
+else 
+	echo "Adding ansible repository and install ansible"; 
+	sudo apt-add-repository ppa:ansible/ansible
+	sudo apt install ansible
+fi
+
 
 ansible-playbook  -i inventory site.yml  -c local --ask-become-pass
